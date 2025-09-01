ba4bb150466dde93db412342df0ac6be41be9a6e
[dyad] <git@dyad.sh>
2025-09-01 13:50:28 +0700

    [dyad] Membersihkan halaman beranda - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,5 @@
 import Navbar from '@/components/layout/Navbar';
 import Hero from '@/components/sections/Hero';
-import Profile from '@/components/sections/Profile';
-import Documents from '@/components/sections/Documents';
-import Programs from '@/components/sections/Programs';
-import Statistics from '@/components/sections/Statistics';
-import Contact from '@/components/sections/Contact';
 import Footer from '@/components/layout/Footer';
 
 const Index = () => {
@@ -13,11 +8,6 @@ const Index = () => {
       <Navbar />
       <main>
         <Hero />
-        <Profile />
-        <Documents />
-        <Programs />
-        <Statistics />
-        <Contact />
       </main>
       <Footer />
     </div>
